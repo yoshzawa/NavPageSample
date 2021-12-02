6e6ac2f4f43252c5d83e87bf18500d89f9f5f317
yoshzawa <yoshzawa@gmail.com>
2021-12-02 16:33:25 +0900

    プロジェクト ファイルを追加します。

---
+++ NavPageSample/MainPage1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavPageSample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage1 : ContentPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MainPage2());
+
+        }
+    }
+}
